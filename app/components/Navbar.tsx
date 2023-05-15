68ba5429479f086b4979ecb8eee1ff486967807a
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-05-15 14:50:43 -0300

    Remove packages about spotify and pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,25 @@
+import Link from 'next/link'
+
 export default function Navbar() {
   return (
     <header className="inline-flex items-center gap-4 w-full justify-between p-3">
-      <h1 className="text-xl font-bold ml-8">BeatBrain</h1>
+      <Link href="/" className="text-xl font-bold ml-8">
+        BeatBrain
+      </Link>
       <div className="inline-flex items-center gap-4">
-        <a
+        <Link
           target="_blank"
-          href="https://www.spotify.com/download/windows/"
+          href="https://www.spotify.com/download/"
           className="text-[#4a2560] w-max px-10 py-3 hover:bg-[#4a2560]/5 active:translate-y-0.5 active:bg-[#3f1f52]/10 outline-none rounded-md font-semibold transition-all duration-200"
         >
           Download the app
-        </a>
-        <a
-          href="/pages/login"
+        </Link>
+        <Link
+          href="/"
           className="w-max px-10 py-3 bg-[#4a2560] hover:bg-[#4a2560]/90 active:translate-y-0.5 active:bg-[#3f1f52] outline-none customShadowMedium text-white rounded-md font-semibold transition-all duration-200"
         >
           Go to the player
-        </a>
+        </Link>
       </div>
     </header>
   )
