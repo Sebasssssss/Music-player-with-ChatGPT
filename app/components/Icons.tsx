c3027dc2a5d635a38c8c6d49665ee16a36469459
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-05-16 21:32:44 -0300

    Simplified icons components

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -1,53 +1,49 @@
-export const IcoSend = () => {
-  return (
-    <svg
-      width="24px"
-      height="24px"
-      viewBox="0 0 24 24"
+export const IcoSend = () => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="currentColor"
+  >
+    <path
+      d="M22 12L3 20l3.563-8L3 4l19 8zM6.5 12H22"
+      stroke="currentColor"
       strokeWidth="1.5"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      color="currentColor"
-    >
-      <path
-        d="M22 12L3 20l3.563-8L3 4l19 8zM6.5 12H22"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      ></path>
-    </svg>
-  )
-}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+)
 
-export const IcoSpotify = () => {
-  return (
-    <svg
-      width="24px"
-      height="24px"
-      viewBox="0 0 24 24"
+export const IcoSpotify = () => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="currentColor"
+  >
+    <path
+      d="M7 15s4.5-1 9 1M6.5 12s6-1.5 11 1.5M6 9c3-.5 8-1 13 2"
+      stroke="currentColor"
       strokeWidth="1.5"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      color="currentColor"
-    >
-      <path
-        d="M7 15s4.5-1 9 1M6.5 12s6-1.5 11 1.5M6 9c3-.5 8-1 13 2"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      ></path>
-      <path
-        d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      ></path>
-    </svg>
-  )
-}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+)
 
 export const IcoPlay = () => (
   <svg
@@ -61,6 +57,26 @@ export const IcoPlay = () => (
   >
     <path
       d="M6.906 4.537A.6.6 0 006 5.053v13.894a.6.6 0 00.906.516l11.723-6.947a.6.6 0 000-1.032L6.906 4.537z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+)
+
+export const IcoSearch = () => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="currentColor"
+  >
+    <path
+      d="M17 17l4 4M3 11a8 8 0 1016 0 8 8 0 00-16 0z"
       stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
