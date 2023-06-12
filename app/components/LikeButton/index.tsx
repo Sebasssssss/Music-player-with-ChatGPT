65f183bc0fd877bf3082e8df7b1fc5eeba931191
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-06-12 19:51:31 -0300

    Change like button colors

--- app/components/LikeButton/index.tsx
+++ app/components/LikeButton/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css'
 export default function LikeButton() {
   return (
-    <button className="w-max h-max">
+    <button className="w-max h-max outline-none">
       <input type="checkbox" id="checkbox" />
       <label for="checkbox">
         <svg
@@ -18,13 +18,13 @@ export default function LikeButton() {
             <path
               d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z"
               id="heart"
-              stroke="#000"
+              stroke="#450a0a"
               strokeWidth={3}
               fill="none"
             />
             <circle
               id="main-circ"
-              fill="#E2264D"
+              fill="#18181b"
               opacity="0"
               cx="29.5"
               cy="29.5"
@@ -32,38 +32,38 @@ export default function LikeButton() {
             />
 
             <g id="grp7" opacity="0" transform="translate(7 6)">
-              <circle id="oval1" fill="#9CD8C3" cx="2" cy="6" r="2" />
-              <circle id="oval2" fill="#8CE8C3" cx="5" cy="2" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="2" cy="6" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="5" cy="2" r="2" />
             </g>
 
             <g id="grp6" opacity="0" transform="translate(0 28)">
-              <circle id="oval1" fill="#CC8EF5" cx="2" cy="7" r="2" />
-              <circle id="oval2" fill="#91D2FA" cx="3" cy="2" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="2" cy="7" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="3" cy="2" r="2" />
             </g>
 
             <g id="grp3" opacity="0" transform="translate(52 28)">
-              <circle id="oval2" fill="#9CD8C3" cx="2" cy="7" r="2" />
-              <circle id="oval1" fill="#8CE8C3" cx="4" cy="2" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="2" cy="7" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="4" cy="2" r="2" />
             </g>
 
             <g id="grp2" opacity="0" transform="translate(44 6)">
-              <circle id="oval2" fill="#CC8EF5" cx="5" cy="6" r="2" />
-              <circle id="oval1" fill="#CC8EF5" cx="2" cy="2" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="5" cy="6" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="2" cy="2" r="2" />
             </g>
 
             <g id="grp5" opacity="0" transform="translate(14 50)">
-              <circle id="oval1" fill="#91D2FA" cx="6" cy="5" r="2" />
-              <circle id="oval2" fill="#91D2FA" cx="2" cy="2" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="6" cy="5" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="2" cy="2" r="2" />
             </g>
 
             <g id="grp4" opacity="0" transform="translate(35 50)">
-              <circle id="oval1" fill="#F48EA7" cx="6" cy="5" r="2" />
-              <circle id="oval2" fill="#F48EA7" cx="2" cy="2" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="6" cy="5" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="2" cy="2" r="2" />
             </g>
 
             <g id="grp1" opacity="0" transform="translate(24)">
-              <circle id="oval1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
-              <circle id="oval2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
+              <circle id="oval1" fill="#450a0a" cx="2.5" cy="3" r="2" />
+              <circle id="oval2" fill="#450a0a" cx="7.5" cy="2" r="2" />
             </g>
           </g>
         </svg>
