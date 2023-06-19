fceb1920cf412a88269920ce7654c1f7b15ef3c0
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-06-19 18:21:43 -0300

    Searches update

--- app/components/ModalContent.tsx
+++ app/components/ModalContent.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import { Search, Cancel } from 'iconoir-react'
 import { AnimatePresence } from 'framer-motion'
 import template from '@/public/album.png'
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 
 interface ModalContentProps {
   isModalOpen: boolean
@@ -14,6 +16,21 @@ export default function ModalContent({
   isModalOpen,
   close
 }: ModalContentProps) {
+  const [input, setInput] = useState('')
+  const [isTyping, setIsTyping] = useState(false)
+
+  const handleChange = e => {
+    setTimeout(() => setInput(e.target.value), 500)
+  }
+
+  useEffect(() => {
+    setIsTyping(false)
+
+    if (input !== '') {
+      setIsTyping(true)
+    }
+  }, [input])
+
   return (
     <AnimatePresence mode="wait" initial={false}>
       {isModalOpen && (
@@ -22,6 +39,7 @@ export default function ModalContent({
             <div className="relative mt-8">
               <input
                 autoFocus={true}
+                onChange={handleChange}
                 placeholder="Search..."
                 className="w-full md:w-96 h-12 px-3 shados rounded-[10px] border-2 hover:border-black/40  active:shadow-none focus:border-black/40 hover:bg-gray-100 transition duration-300 focus:outline-none input"
               />
@@ -29,28 +47,34 @@ export default function ModalContent({
                 <Search className="text-gray-500" />
               </button>
             </div>
-            <div className="bg-white w-full h-full rounded-[10px] shados relative">
-              <div className="flex flex-col items-center justify-between shados gap-8 p-8 w-full rounded-[10px] relative">
-                <div className="flex flex-col gap-2">
-                  <Image
-                    className="rounded-[10px] shados"
-                    src={template}
-                    width={200}
-                    height={200}
-                    alt=""
-                  />
-                  <div className="flex flex-col gap-4 text-center">
-                    <h1 className="font-bold text-4xl">Ghost</h1>
-                    <h1 className="text-zinc-400">Jaden Smith</h1>
+            {isTyping ? (
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                className="bg-white w-full h-full rounded-[10px] shados relative"
+              >
+                <div className="flex flex-col items-center justify-between shados gap-8 p-8 w-full rounded-[10px] relative">
+                  <div className="flex flex-col gap-2">
+                    <Image
+                      className="rounded-[10px] shados"
+                      src={template}
+                      width={200}
+                      height={200}
+                      alt=""
+                    />
+                    <div className="flex flex-col gap-4 text-center">
+                      <h1 className="font-bold text-4xl">Ghost</h1>
+                      <h1 className="text-zinc-400">Jaden Smith</h1>
+                    </div>
                   </div>
+                  <ListOfSongs />
                 </div>
-                <ListOfSongs />
-              </div>
-              <Cancel
-                onClick={close}
-                className="absolute top-4 right-4 cursor-pointer"
-              />
-            </div>
+                <Cancel
+                  onClick={close}
+                  className="absolute top-4 right-4 cursor-pointer"
+                />
+              </motion.div>
+            ) : null}
           </div>
         </Modal>
       )}
