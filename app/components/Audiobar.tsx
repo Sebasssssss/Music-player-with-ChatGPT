b0c72f25d5cae5b877c06ffda1eda55b22706394
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-06-22 15:40:54 -0300

    Change audiobar design

--- app/components/Audiobar.tsx
+++ app/components/Audiobar.tsx
@@ -1,5 +1,13 @@
 'use client'
-import { SoundHigh, Pause, Play, SoundOff, Playlist } from 'iconoir-react'
+import {
+  SoundHigh,
+  Pause,
+  Play,
+  SoundOff,
+  Playlist,
+  SkipPrev,
+  SkipNext
+} from 'iconoir-react'
 import { useAudioContext } from '../providers/AppState'
 import ChatTrigger from './ChatTrigger'
 
@@ -21,28 +29,39 @@ export default function Audiobar() {
       <audio ref={audioRef} onTimeUpdate={handleTimeUpdate}>
         <source src="likeyou.mp3" type="audio/mpeg" />
       </audio>
-      <div className="w-full flex items-center gap-8">
-        <button
-          type="button"
-          onClick={handleTogglePlay}
-          className="outline-none"
-        >
-          {pause ? <Play /> : <Pause />}
-        </button>
-        <div className="w-full flex items-center gap-2">
-          <p className="font-medium text-xs">0:00</p>
-          <input
-            type="range"
-            min="0"
-            step="0.1"
-            max={audioRef.current?.duration || 0}
-            value={currentTime}
-            onChange={handleSeek}
-            className="range-slider w-full appearance-none bg-gray-200 h-1 rounded-lg focus:outline-none active:bg-gray-300"
-          />
-          <p className="font-medium text-xs">3:30</p>
+      <div className="w-full grid grid-cols-3 gap-8">
+        <div />
+        <div className="w-full flex flex-col gap-2 items-center justify-center">
+          <div className="flex items-center gap-4">
+            <SkipPrev />
+            <button
+              type="button"
+              onClick={handleTogglePlay}
+              className="outline-none"
+            >
+              {pause ? (
+                <Play className="w-8 h-8" />
+              ) : (
+                <Pause className="w-8 h-8" />
+              )}
+            </button>
+            <SkipNext />
+          </div>
+          <div className="w-full flex items-center gap-2">
+            <p className="font-medium text-xs">0:00</p>
+            <input
+              type="range"
+              min="0"
+              step="0.1"
+              max={audioRef.current?.duration || 0}
+              value={currentTime}
+              onChange={handleSeek}
+              className="range-slider w-full appearance-none bg-gray-200 h-1 rounded-lg focus:outline-none active:bg-gray-300"
+            />
+            <p className="font-medium text-xs">3:30</p>
+          </div>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 justify-end">
           <ChatTrigger />
           <button className="disabled:opacity-40" disabled>
             <Playlist />
